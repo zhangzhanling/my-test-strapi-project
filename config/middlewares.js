03f9dec4d2660acf818224918f87f1be0ebbff60
张占岭 <1028167362@qq.com>
2025-08-02 16:07:03 +0800

    feat: 添加博客和动物内容类型及多语言支持
    - 新增博客内容类型，支持多语言和富文本编辑
    - 添加动物内容类型用于基础数据管理
    - 配置AWS S3文件上传插件
    - 更新中间件配置以支持S3资源加载
    - 添加Localazy插件支持多语言翻译
    - 完善类型定义文件

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'most-strapi-bucket.s3.ap-southeast-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'most-strapi-bucket.s3.ap-southeast-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,37 @@
-module.exports = () => ({});
+
+module.exports = ({ env }) => {
+    console.log('---AWS S3 CONFIG 区域---', env('AWS_REGION'))
+    console.log('---AWS S3 CONFIG 桶名称---', env('AWS_BUCKET'))
+    console.log('---AWS S3 CONFIG 密钥 id---', env('AWS_ACCESS_KEY_ID'))
+    console.log('---AWS S3 CONFIG 密钥 key---', env('AWS_ACCESS_SECRET'))
+    return {
+        upload: {
+            config: {
+                provider: 'aws-s3',
+                providerOptions: {
+                    s3ForcePathStyle: true,
+                    s3Options: {
+                        credentials: {
+                            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                            secretAccessKey: env('AWS_ACCESS_SECRET'),
+                        },
+                        region: env('AWS_REGION', 'ap-southeast-2'),
+                        params: {
+                            Bucket: env('AWS_BUCKET'),
+                            ACL: null,
+                        },
+                    },
+                    actionOptions: {
+                        upload: {
+                            ACL: null,
+                        },
+                        uploadStream: {
+                            ACL: null,
+                        },
+                        delete: {},
+                    },
+                }
+            },
+        },
+    }
+};
